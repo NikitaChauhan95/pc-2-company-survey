a54f12a3e7f0ba364819d51e784a5f6b67b1ddfb
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-01 18:06:03 +0530

    passed the test cases

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -8,9 +8,20 @@ public class AverageAge {
 
         try{
             //Write the logic
+          float [] ageRawDataDouble = new float[ageRawData.length];
+            double sum = 0;
+          for (int index = 0; index < ageRawData.length; index++) {
+              ageRawDataDouble[index] = Float.parseFloat(ageRawData[index]);
+              //avg = sum of numbers / total number
+
+              sum += ageRawDataDouble[index];
+              //sum++;
+              avg = sum / ( ageRawDataDouble.length);
+
+          }
 
             //Handle specific exception
-        }catch (Exception exception)
+        }catch (NumberFormatException exception)
         {
             return exception.toString();
         }
